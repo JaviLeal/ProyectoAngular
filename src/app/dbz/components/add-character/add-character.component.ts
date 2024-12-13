1313d95cb13773ff06097f0e1f5d5fab6926aaab
JaviLeal <157105493+JaviLeal@users.noreply.github.com>
2024-12-13 09:51:40 +0100

    Clase add-character.component.ts comentada

--- src/app/dbz/components/add-character/add-character.component.ts
+++ src/app/dbz/components/add-character/add-character.component.ts
@@ -3,25 +3,25 @@ import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'dbz-add-character',
-  templateUrl: './add-character.component.html',
+  templateUrl: './add-character.component.html', 
   styleUrl: './add-character.component.css'
 })
 export class AddCharacterComponent {
  @Output()
- public onNewCharacter: EventEmitter<Character> = new EventEmitter();
+ public onNewCharacter: EventEmitter<Character> = new EventEmitter(); // Emite el evento cuando se agrega un nuevo personaje
 
-    public character: Character = {
+    public character: Character = {  // Almacena el personaje con sus datos (esos serán sus valores iniciales)
       name:'',
       power: 0
     };
 
     emitCharacter():void{
 
-      if (this.character.name.length === 0) return;
+      if (this.character.name.length === 0) return; // Si el nombre está vacío, no hace nada
 
-      this.onNewCharacter.emit(this.character);
+      this.onNewCharacter.emit(this.character); // Emite el personaje 
 
-      this.character={ name: '', power:0};
+      this.character={ name: '', power:0}; // Reinicia el objeto una vez se envíe el personaje
 
     }
 }
