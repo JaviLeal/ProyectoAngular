7b43fda2866fe8621a9f495c2d86a428c2e200af
JaviLeal <157105493+JaviLeal@users.noreply.github.com>
2024-12-13 10:27:35 +0100

    Clase main-page.component.ts comentada

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -2,14 +2,14 @@ import { Character } from './../interfaces/character.interface';
 import { Component} from '@angular/core';
 
 
-@Component({
+@Component({  // Decorador que define el componente con su selector y plantilla correspondiente
   selector: 'app-dbz-main-page',
   templateUrl: './main-page.component.html'
 })
 
 export class MainPageComponent {
 
-  public characters: Character[] = [{
+  public characters: Character[] = [{  // Array que contiene la lista de los personajes y sus características
     name: 'Krillin',
     power: 1000
   },{
@@ -22,11 +22,11 @@ export class MainPageComponent {
 
  // onDelete = Index value: number;
 
-  onNewCharacter(character:Character): void{
+  onNewCharacter(character:Character): void{  // Método que agrega un nuevo personaje a la lista
     this.characters.push(character);
   }
 
-  onDeleteCharacter(index:number){
+  onDeleteCharacter(index:number){  // Método para eliminar un personaje de la lista basándose en su índice
 
     //this.characters.splice(1,2)
     this.characters.splice(index,1);
